2e6e58a510ffc4201caf2ed85027b39fadb6e106
Ali Shabanzade <shabanzade.ali@gmail.com>
2023-01-19 00:24:36 +0330

    02-file-managment-function

--- PHP-TOOL-BOX/10-file-in-php/01-file-managment-functions.php
+++ PHP-TOOL-BOX/10-file-in-php/01-file-managment-functions.php
@@ -44,7 +44,7 @@ $dirPath1 = "test-directory/test2.txt";
 // The fopen() function opens a file or URL
 $fileOpen = fopen($dirPath1 , 'a+');
 // The fwrite() writes to an open file
-fwrite($fileOpen, "ali shabanzade");
+fwrite($fileOpen, "ali shabanzade" . PHP_EOL);
 // The fclose() function closes an open file pointer
 fclose($fileOpen);
 
@@ -54,5 +54,51 @@ $dirPath2 = "test-directory/logs.txt";
 $writeFile = fopen($dirPath2 , 'a+');
 for($i = 1 ; $i<10 ; $i++){
     //PHP_EOL go to new line
-    fwrite($writeFile , "ali shabanzade" . PHP_EOL);
+    fwrite($writeFile , "Ali Shabanzade" . PHP_EOL);
 }
+fclose($writeFile);
+echo "<hr>";
+
+
+$dirPath2 = "test-directory/logs.txt";
+$writeFile = fopen($dirPath2 , 'a+');
+for($i = 1 ; $i<7; $i++){
+    // The fgetc() function returns a single character from an open file
+    $getChar = fgetc($writeFile);
+    echo $getChar . "<br>";
+   
+    
+}
+fclose($writeFile);
+echo "<hr>";
+
+
+$dirPath2 = "test-directory/logs.txt";
+$writeFile = fopen($dirPath2 , 'a+');
+for($i = 1 ; $i<3; $i++){
+    // The fgets() function returns a line from an open file
+    $getLine = fgets($writeFile );
+    echo $getLine . "<br>";
+    
+}
+fclose($writeFile);
+
+echo "<hr>";
+
+$dirPath3 = "test-directory/test3.txt";
+$str = "this is new text" . PHP_EOL;
+// The file_put_contents() writes data to a file
+file_put_contents($dirPath3 , $str );
+// FILE_APPEND - if file already exists, append the data to it - instead of overwriting it
+file_put_contents($dirPath2 , $str , FILE_APPEND);
+
+
+//
+$getContent = file_get_contents($dirPath1 );
+echo $getContent;
+echo "<hr>";
+echo nl2br($getContent);
+
+
+
+
