f9890e511d3a5dfeb7aee8cd59f19f7dd5e48c36
Ali Shabanzade <shabanzade.ali@gmail.com>
2023-01-30 19:11:17 +0330

    connect php to database with msqli

---
+++ 14-php-mysql-drivers/01-mysqli/01-connection.php
@@ -0,0 +1,21 @@
+<?php
+
+//create mysql connection whit mysqli driver
+$mysqli = new mysqli("localhost" , "root" , "" , "world");
+
+//check connection is ok
+if($mysqli->connect_errno){
+    echo "Failed to connect to Mysql . error is :" . $mysqli->errno;
+    
+}else{
+    echo "successfully connected to mysql";
+}
+
+
+//set characters for showing persian properly
+$mysqli->set_charset('utf8');
+
+
+// ?for analyze mysql connection error
+print_r($mysqli);
+var_dump($mysqli);
