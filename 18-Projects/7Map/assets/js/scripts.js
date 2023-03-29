f2473902e16504e99a982796b3cd01322c9ab2e6
Ali Shabanzade <shabanzade.ali@gmail.com>
2023-03-29 20:52:27 +0330

    add geolocation api

--- 18-Projects/7Map/assets/js/scripts.js
+++ 18-Projects/7Map/assets/js/scripts.js
@@ -1,106 +1,114 @@
-const defaultLocation = [35.722, 51.328];
+
+
+const defaultLocation = [36.312875, 59.527160];
 const defaultZoom = 15;
+//set location by default L is a object of leaflet
+//map is variable and - ('map') is div tag from index
 var map = L.map('map').setView(defaultLocation, defaultZoom);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    attribution: '7Learn.ac <a href="https://7learn.ac/course-php-expert">7Map Project</a> ',
+    attribution: 'Map Project by Ali Shabanzade <a href="https://7learn.com/course/php-expert">7Learn.com-php-expert</a>',
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1
 }).addTo(map);
+
+//set dynamic height full screen by js
 document.getElementById('map').style.setProperty('height', window.innerHeight + 'px');
 
-// set view in map
-// map.setView([29.592, 52.573], defaultZoom);
+//set a new location
+//     map.setView([35.7048585,51.3617731],defaultZoom);
+
+//??
+// set marker and popup information
+// L.marker(defaultLocation).addTo(map).bindPopup("Ferdowsi University of Mashhad").openPopup();
 
 
-// show and pin markers
-// L.marker(defaultLocation).addTo(map).bindPopup("7Learn Office 1").openPopup();
-// L.marker([35.712, 51.338]).addTo(map).bindPopup("7Learn Office 2");
-// map.on('popupopen', function() {
-//     alert('PopUp Opened!');
+//??set second marker location
+//     L.marker([36.3214213,59.5354162]).addTo(map).bindPopup("Mellat Amusement Park");
+
+//     map.on('popupopen' , function(){
+//         alert('this is a popup opened');
+//    });
+
+
+//??get view Bound information
+// var northLine = map.getBounds().getNorth(); // north Line
+// var westhLine = map.getBounds().getWest();  //west Line
+// var soutLine = map.getBounds().getSouth();  //sout Line
+// var eastLine = map.getBounds().getEast(); // east Line
+
+// map.setView([northLine,westhLine],defaultZoom);
+
+// ?? use Map Events
+// map.on('zoomend',function(){
+//     alert(map.getBounds().getCenter());
+/*
+1 : get bound lines (north,south,west,east)
+2 : send bound lines to server
+3 : search locations in map windows(active window)
+4 : diplay loction markers in map
+*/
 // });
 
-// get view Bound information
-// var northLine = map.getBounds().getNorth(); // khat shomali
-// var westLine = map.getBounds().getWest(); // khat Gharbi
-// var southLine = map.getBounds().getSouth(); // khat Jonoobi
-// var eastLine = map.getBounds().getEast(); // khat Sharghi
 
 
-// Use Map Events
-map.on('zoomend', function() {
-    // alert(map.getBounds().getCenter());
-    // 1 : get bound lines
-    // 2 : send bound lines to server
-    // 3 : search locations in map windows
-    // 4 : display location markers in map
-});
-
-// Use Map Events
-map.on('dblclick', function(event) {
-    // alert(event.latlng.lat + " , " + event.latlng.lng);
-    // 1: add marker in clicked position
-    L.marker(event.latlng).addTo(map);
-    // 2: open modal (form) for save the clicked location
-    $('.modal-overlay').fadeIn(500);
-    $('#lat-display').val(event.latlng.lat);
-    $('#lng-display').val(event.latlng.lng);
-    $('#l-type').val(0);
-    $('#l-title').val('');
-    // 3: fill the form and submit location data to server
-
-    // 4: save location in database (status : pending review)
-    // 5: review locations and verify if OK
-});
+//   setTimeout(function(){
 
 
+//   }, 2000);
 
 
+// map.on('dblclick' , function(event){
+// The log() method writes (logs) a message to the console.
+//    console.log(event);
+//   add a marker where duble click happened
+//   L.marker([event.latlng.lat , event.latlng.lng]).addTo(map); 
+//   alert(event.latlng.lat + " , " + event.latlng.lng);
+/*
+1 : add marker in ckicked position
+2 : open modal (form) for save the clicked location
+3 : fill the form and submit location data to server
+4 : save location in database (status : pending review)
+5 : review locations and verify if OK
+*/
+// });
 
-// find Current Location (at first, Use Shekan.ir)
+
 var current_position, current_accuracy;
-map.on('locationfound', function(e) {
-    // if position defined, then remove the existing position marker and accuracy circle from the map
+map.on('locationfound', function(e){
+    // if position defined, then remove the existing position marker and accuracy circle from the map 
     if (current_position) {
         map.removeLayer(current_position);
         map.removeLayer(current_accuracy);
     }
+
     var radius = e.accuracy;
     current_position = L.marker(e.latlng).addTo(map)
-        .bindPopup("دقت تقریبی: " + radius + " متر").openPopup();
-    current_accuracy = L.circle(e.latlng, radius).addTo(map);
+    .bindPopup("دقت تقریبی: " + radius + " متر").openPopup();
+    current_accuracy = L.circle(e.latlng , radius).addTo(map);
+
 });
-map.on('locationerror', function(e) {
+
+map.on('locationerror', function(e){
     alert(e.message);
 });
-// wrap map.locate in a function    
+
+
 function locate() {
     map.locate({ setView: true, maxZoom: defaultZoom });
 }
-// call locate every 5 seconds... forever
+
 // setInterval(locate, 5000);
 
 
 
-$(document).ready(function() {
-    $('form#addLocationForm').submit(function(e) {
-        e.preventDefault(); // prevent form submiting
-        var form = $(this);
-        var resultTag = form.find('.ajax-result');
-        $.ajax({
-            url: form.attr('action'),
-            method: form.attr('method'),
-            data: form.serialize(),
-            success: function(response) {
-                resultTag.html(response);
-            }
-        });
-    });
 
 
-    $('.modal-overlay .close').click(function() {
-        $('.modal-overlay').fadeOut();
-    });
-});
+
+
+
+
+
+
--- 18-Projects/7Map/index.php
+++ 18-Projects/7Map/index.php
@@ -7,12 +7,13 @@
     <title>7Map</title>
     <link href="favicon.png" rel="shortcut icon" type="image/png">
 
-    <link rel="stylesheet" href="assets/css/leaflet.css" />
     <link rel="stylesheet" href="assets/css/styles.css" />
 
-    <script src="assets/js/leaflet.js">
-         
-    </script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+
+
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
 
 </head>
 
@@ -26,53 +27,8 @@
         </div>
     </div>
 
-    <script>
-    
-    //set location by default L is a object of leaflet
-	const mymap = L.map('map').setView([36.312875, 59.527160], 15);
-    //loading tilelayer 
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: 'Map Project by Ali Shabanzade <a href="https://7learn.com/course/php-expert">7Learn.com-php-expert</a>'
-	}).addTo(mymap);
-
-    //set dynamic height full screen by js
-    document.getElementById('map').style.setProperty('height', window.innerHeight + 'px');
-
-	// const marker = L.marker([51.5, -0.09]).addTo(map)
-	// 	.bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
-
-	// const circle = L.circle([51.508, -0.11], {
-	// 	color: 'red',
-	// 	fillColor: '#f03',
-	// 	fillOpacity: 0.5,
-	// 	radius: 500
-	// }).addTo(map).bindPopup('I am a circle.');
-
-	// const polygon = L.polygon([
-	// 	[51.509, -0.08],
-	// 	[51.503, -0.06],
-	// 	[51.51, -0.047]
-	// ]).addTo(map).bindPopup('I am a polygon.');
-
-
-	// const popup = L.popup()
-	// 	.setLatLng([51.513, -0.09])
-	// 	.setContent('I am a standalone popup.')
-	// 	.openOn(map);
-
-	// function onMapClick(e) {
-	// 	popup
-	// 		.setLatLng(e.latlng)
-	// 		.setContent(`You clicked the map at ${e.latlng.toString()}`)
-	// 		.openOn(map);
-	// }
-
-	// map.on('click', onMapClick);
-
-</script>
+    <script src="assets/js/scripts.js"></script>
 
 </body>
 
 </html>
-       
